51f8ccda5901f2204698b4d015df73ab8710d31e
ystyle0701 <info@ystyle-corp.com>
2026-03-18 09:59:39 +0900

    Add print() 2 problem

---
+++ python/basics/001_print_statement1/answer.py
@@ -0,0 +1,19 @@
+#No.1
+print('田中ありさ')
+
+#No.2
+print('田中')
+print('ありさ')
+
+#No.3
+print('名前:佐藤')
+print('年齢:25')
+print('趣味:音楽を聴くこと')
+
+#No.4
+print('Python!'+'Python!'+'Python!')
+
+#No.5
+print('>', end='')
+print('_', end='')
+print('<')
---
+++ python/basics/002_print_statement2/answer.py
@@ -0,0 +1,19 @@
+# No.1
+print("   A  ")
+print("  A A ")
+print(" A   A")
+print(" AAAAA")
+print(" A   A")
+print(" A   A")
+print(" A   A")
+
+print()
+
+# No.2
+print("  SSS   L     ")
+print(" S   S  L     ")
+print(" S      L     ")
+print("  SSS   L     ")
+print("     S  L     ")
+print(" S   S  L     ")
+print("  SSS   LLLLL ")
